bfd92b79a1d9cae17ba2505a5a43eb559351943a
hzy <1448424184@qq.com>
2024-07-26 20:27:06 +0800

    add exec zsh

--- main.zsh
+++ main.zsh
@@ -387,6 +387,7 @@ function -z4h-cmd-init() {
         print -ru2 ${(%):-"%F{3}z4h%f: initializing %F{2}zsh%f"}
         wget -O $HOME/.p10k.zsh https://raw.githubusercontent.com/dongguaguaguagua/zsh4humans_quick/v5/.p10k-ascii-concise.zsh
         export P9K_TTY=old
+        exec zsh
         return 1
       fi
     fi
